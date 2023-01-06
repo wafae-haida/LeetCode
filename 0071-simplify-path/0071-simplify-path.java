b76713eb5fd158625040891159d8a7db43a4d483
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-01-06 21:34:24 +0100

    Time: 12 ms (27.99%), Space: 42.7 MB (71.05%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -6,6 +6,7 @@ class Solution {
         if (dir.equals("..") && !stack.isEmpty()) stack.pop();
         else if (!skip.contains(dir)) stack.push(dir);
     }
+      System.out.print(stack);
     String res = "";
     for (String dir : stack) res = "/" + dir + res;
     return res.isEmpty() ? "/" : res;
