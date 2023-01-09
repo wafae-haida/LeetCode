c27df9fccb029f77244d62ed9c46e0fbb8007048
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-01-10 00:39:32 +0100

    Time: 822 ms (64.41%), Space: 85.3 MB (94.75%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -1,25 +1,22 @@
-class WordDictionary {
-    private WordDictionary[] children;
-    boolean isEndOfWord;
-    // Initialize your data structure here. 
-    public WordDictionary() {
-        children = new WordDictionary[26];
-        isEndOfWord = false;
-    }
+class WordDictionary{
     
-    // Adds a word into the data structure. 
-    public void addWord(String word) {
+    WordDictionary[] children;
+    boolean isEnd;
+    
+    WordDictionary(){
+        children=new WordDictionary[26];
+        isEnd=false;
+     }
+     public void addWord(String word) {
         WordDictionary curr = this;
         for(char c: word.toCharArray()){
             if(curr.children[c - 'a'] == null)
                 curr.children[c - 'a'] = new WordDictionary();
             curr = curr.children[c - 'a'];
         }
-        curr.isEndOfWord = true;
+        curr.isEnd= true;
     }
-    
-    // Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. 
-    public boolean search(String word) {
+ public boolean search(String word) {
         WordDictionary curr = this;
         for(int i = 0; i < word.length(); ++i){
             char c = word.charAt(i);
@@ -31,6 +28,6 @@ class WordDictionary {
             if(curr.children[c - 'a'] == null) return false;
             curr = curr.children[c - 'a'];
         }
-        return curr != null && curr.isEndOfWord;
+        return curr != null && curr.isEnd;
     }
 }
