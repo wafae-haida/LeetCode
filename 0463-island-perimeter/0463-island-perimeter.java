c650abece31c8b984ef5a16134117ea5011a8954
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-01-17 12:01:08 +0100

    Time: 9 ms (68.27%), Space: 43.4 MB (60.30%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -8,33 +8,35 @@
     
 */
 
-class Solution {
+public class Solution {
     public int islandPerimeter(int[][] grid) {
-        int ans=0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[i].length;j++){
-                if(j-1>=0 ){
-                  if(grid[i][j-1]==0 && grid[i][j]==1)ans++;
-                }else if(j-1<0 && grid[i][j]==1){
-                    ans++;
-                }   
-                if(j+1<grid[i].length){
-                    if(grid[i][j+1]==0 && grid[i][j]==1)ans++;
-                }else if(j+1>=grid[i].length && grid[i][j]==1){
-                    ans++;
-                }
-                if(i+1<grid.length){
-                    if(grid[i+1][j]==0 && grid[i][j]==1)ans++;
-                }else if(i+1>=grid.length && grid[i][j]==1){
-                    ans++;
-                }
-                if(i-1>=0){
-                    if(grid[i-1][j]==0 && grid[i][j]==1)ans++;
-                }else if(i-1<0 && grid[i][j]==1){
-                    ans++;
+        if (grid == null) return 0;
+        for (int i = 0 ; i < grid.length ; i++){
+            for (int j = 0 ; j < grid[0].length ; j++){
+                if (grid[i][j] == 1) {
+                    return getPerimeter(grid,i,j);
                 }
             }
         }
-        return ans;
+        return 0;
+    }
+    
+    public int getPerimeter(int[][] grid, int i, int j){
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) {return 1;}
+        if (grid[i][j] == 0) {
+            return 1;
+        }
+        if (grid[i][j] == -1) return 0;
+        
+        int count = 0;
+        grid[i][j] = -1;
+        
+        count += getPerimeter(grid, i-1, j);
+        count += getPerimeter(grid, i, j-1);
+        count += getPerimeter(grid, i, j+1);
+        count += getPerimeter(grid, i+1, j);
+        
+        return count;
+        
     }
 }
