c50c2b766297b258c5d26265cf81b8bd119ac6a9
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-01-20 14:38:23 +0100

    Time: 1 ms (88.37%), Space: 40.2 MB (88.09%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -0,0 +1,23 @@
+class Solution{
+    static String ans="";
+    public String findDifferentBinaryString(String[] nums) {
+        HashSet<String> dict=new HashSet<>();
+        for(String s:nums)
+            dict.add(s);
+        solve(0,nums.length,dict,"");
+        return ans;
+    }
+    
+    public static boolean solve(int ci,int ti, HashSet<String> dict,String asf){
+        if(ci==ti){
+            if(!dict.contains(asf)){
+                ans=asf;
+                return true; 
+            }
+            return false;
+        }
+        if(solve(ci+1,ti,dict,asf+1))return true;
+        if(solve(ci+1,ti,dict,asf+0))return true;
+        return false;
+    }
+}  
