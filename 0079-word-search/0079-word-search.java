9b24d8969c4813f05d9c2511a2ff4569e4ee2169
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-01-20 10:39:13 +0100

    Time: 212 ms (35.50%), Space: 40.3 MB (90.48%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -14,31 +14,16 @@ class Solution {
         return false;
     }
 
-    public boolean check(
-        char[][] board,
-        String word,
-        int i,
-        int j,
-        int m,
-        int n,
-        int cur
-    ) {
+    public boolean check(char[][] board,String word,int i,int j,int m,int n,int cur) {
         if (cur >= word.length()) return true;
-        if (
-            i < 0 ||
-            j < 0 ||
-            i >= m ||
-            j >= n ||
-            board[i][j] != word.charAt(cur)
-        ) return false;
+        if (i < 0 ||j < 0 ||i >= m ||j >= n ||board[i][j] != word.charAt(cur)) return false;
         boolean exist = false;
         if (board[i][j] == word.charAt(cur)) {
-            board[i][j] += 100;
-            exist =
-                check(board, word, i + 1, j, m, n, cur + 1) ||
-                check(board, word, i, j + 1, m, n, cur + 1) ||
-                check(board, word, i - 1, j, m, n, cur + 1) ||
-                check(board, word, i, j - 1, m, n, cur + 1);
+            board[i][j]+=100;
+            exist = check(board, word, i + 1, j, m, n, cur + 1) ||
+                    check(board, word, i, j + 1, m, n, cur + 1) ||
+                    check(board, word, i - 1, j, m, n, cur + 1) ||
+                    check(board, word, i, j - 1, m, n, cur + 1);
             board[i][j] -= 100;
         }
         return exist;
