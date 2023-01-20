59dff0f8416317a968eb8d74f03df30f1a3ff65d
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-01-20 14:51:00 +0100

    Time: 2 ms (63.99%), Space: 40.4 MB (77.56%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -1,24 +1,22 @@
-
-class Solution{
-     String ans="";
+class Solution {
+    String ans;
     public String findDifferentBinaryString(String[] nums) {
-        HashSet<String> dict=new HashSet<>();
-        for(String s:nums)
-            dict.add(s);
-        solve(0,nums.length,dict,"");
+        if(nums==null || nums.length==0) return "";
+        Set<String> set = new HashSet<>();
+        for(String s:nums)set.add(s);
+        dfs(set,0,"",nums.length);
         return ans;
     }
     
-    public  boolean solve(int ci,int ti, HashSet<String> dict,String asf){
-        if(ci==ti){
-            if(!dict.contains(asf)){
-                ans=asf;
-                return true; 
-            }
-            return false;
+    boolean dfs(Set<String> set,int pos, String curr, int length){
+        if(pos==length){
+            if(set.contains(curr))return false;
+            else{ans=curr;
+                return true;}
         }
-        if(solve(ci+1,ti,dict,asf+1))return true;
-        if(solve(ci+1,ti,dict,asf+0))return true;
+        if(dfs(set,pos+1,curr+0,length))return true;
+        if(dfs(set,pos+1,curr+1,length))return true;
         return false;
     }
-}  
+}
+
