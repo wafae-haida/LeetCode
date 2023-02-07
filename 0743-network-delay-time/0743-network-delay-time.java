fabeda20c0d89e433ab9fad5518e004019a05e86
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-02-07 11:33:32 +0100

    Time: 15 ms (71.67%), Space: 46.5 MB (52.56%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -4,15 +4,13 @@ class Solution {
         Arrays.fill(path,Integer.MAX_VALUE);
         path[k-1]=0;
         for(int j=0;j<n;j++){
-            int[] pathcopy=Arrays.copyOf(path,n);
             for(int i=0;i<times.length;i++){
                 int src=times[i][0];
                 int trgt=times[i][1];
                 int time=times[i][2];
-                if(pathcopy[src-1]!=Integer.MAX_VALUE && pathcopy[src-1]+time<pathcopy[trgt-1])
-                    pathcopy[trgt-1]=pathcopy[src-1]+time;
+                if(path[src-1]!=Integer.MAX_VALUE && path[src-1]+time<path[trgt-1])
+                    path[trgt-1]=path[src-1]+time;
             }
-            path=pathcopy;
         }
         int ans=Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
