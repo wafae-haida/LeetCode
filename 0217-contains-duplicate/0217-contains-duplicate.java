5901722b94840aa104e65cec551b64d3bbea7a0e
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-02-11 16:23:43 +0100

    Time: 19 ms (42.20%), Space: 56.7 MB (32.85%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int j=1;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[j++])return true;
+        }
+        return false;
+    }
+}
