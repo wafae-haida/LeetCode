c2ad736564ce63af46414f8d5318cc7343407ea3
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-02-13 11:25:46 +0100

    Time: 4 ms (46.91%), Space: 42.4 MB (54.63%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,27 @@
+/*
+nums2[0,1,2,6,8,10]->original
+nums1[1,0,2,8,2,6]
+
+since the nums2 is sorted
+binary search O(logn)
+
+*/
+class Solution {
+    public int binarySearch(int[] nums,int i){
+        for(int n=0; n<nums.length; n++){
+            if(nums[n]==i){
+                for(int j=n+1; j<nums.length; j++){
+                    if(nums[j]>i)
+                        return nums[j];
+                }
+            }
+        }
+        return -1;
+    }
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+       for(int i=0;i<nums1.length;i++){
+           nums1[i]=binarySearch(nums2,nums1[i]);
+       }
+        return nums1;
+    }
+}
