059b65b75493e80446d787810f949fc169a4461c
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-02-15 17:05:21 +0100

    Time: 657 ms (26.48%), Space: 43.4 MB (35.47%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -14,15 +14,11 @@
 
 */
 class Solution {
-    public boolean checkPairs(char start, char end){
-        if(start==end)return true;
-        return false;
-    }
     public boolean isPalindrome(String s) {
         s=s.toLowerCase();
         s=s.replaceAll("[^a-zA-Z0-9]","");
         for(int i=0,j=s.length()-1;i<s.length() && j>=0;j--,i++)
-            if(!checkPairs(s.charAt(i),s.charAt(j)))return false;
+            if(s.charAt(i)!=s.charAt(j))return false;
         return true;
     }
 }
