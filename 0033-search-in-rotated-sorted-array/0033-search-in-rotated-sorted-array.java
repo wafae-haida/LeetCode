a7bc2e0df20d05e2b224e2fcf420ab95fda7c1c8
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-02-16 23:18:01 +0100

    Time: 0 ms (100.00%), Space: 41.8 MB (78.79%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,24 +1,24 @@
 class Solution {
-    public int search(int[] A, int target) {
-        int left = 0;
-        int right = A.length - 1;
-        while (left < right) {
-            int mid = left+(right-left)/ 2;
-            if (A[mid] == target) return mid;
-        
-            if (A[left] <= A[mid]) {
-                if (target >= A[left] && target < A[mid])
-                    right = mid - 1;
+    public int search(int[] nums, int target) {
+       int left=0,right=nums.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(target==nums[mid])
+                return mid;
+            if(nums[mid]>=nums[left]){
+                if(target==nums[left])
+                    return left;
+                if(target>nums[left] && target<nums[mid])
+                    right=mid-1;
                 else
-                    left = mid + 1;
-                
-            } else {
-                if (target > A[mid] && target <= A[right])
-                    left = mid + 1;
-            else 
-                right = mid - 1;
+                    left=mid+1;
+            }else{
+                if(target<=nums[right] && target>nums[mid])
+                    left=mid+1;
+                else
+                    right=mid-1;
+            }
         }
-    }
-    return A[right] == target ? left : -1;
+        return -1;
     }
 }
