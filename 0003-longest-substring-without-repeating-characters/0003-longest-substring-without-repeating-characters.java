2808ca2f6e6f73837679b6044f374efcc62b2838
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-02-16 12:48:21 +0100

    Time: 7 ms (69.40%), Space: 42.4 MB (73.23%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -0,0 +1,31 @@
+/*
+"exampleexample"
+""
+"example"
+"152++"
+"ex145*4"
+"++-*"
+"EXAM  pLE"
+ 
+ Target: return the longest distinct substring
+
+*/
+class Solution {
+
+    public int lengthOfLongestSubstring(String s) {
+            List<Character> substringL = new ArrayList<>();
+            int largestlength = 0;
+            for(int right = 0; right < s.length(); right++) {
+                if(substringL.contains(s.charAt(right))) { 
+                    // get the index of the char
+                    int index = substringL.indexOf(s.charAt(right));
+                    substringL.remove(index);
+                    if(index > 0)
+                        substringL.subList(0, index).clear();
+                }
+                substringL.add(s.charAt(right));
+                largestlength = Math.max(largestlength, substringL.size());
+            }
+            return largestlength;
+    }
+}
