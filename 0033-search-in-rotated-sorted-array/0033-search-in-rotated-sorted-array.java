ff5cfcadba68a31e48dc1bcee7fa08d3d7d5d2e7
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-02-16 22:53:02 +0100

    Time: 0 ms (100.00%), Space: 42 MB (45.15%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,16 +1,3 @@
-//ascending order, distinct 
-//nums[mid]<nums[mid+1] && nums[mid]>nums[mid-1] => [0,1,2]4[5,6,7]
-//                                               => [7,6,0]2[3,4,5]
-//                                                                 =>mid--
-
-
-//nums[mid]<nums[mid+1] && nums[mid]<nums[mid-1] => not possible
-//nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1] => [4,5,6]7[0,1,2] Or [0,1,2,4,5,6,7] 
-//                                                                  ->Ok
-//nums[mid]>nums[mid+1] && nums[mid]<nums[mid-1] => not possible
-
-//search target int rotated nums => O(log n) 
-
 class Solution {
     public int search(int[] A, int target) {
         int start = 0;
