dceb01721e1016fcee1474a929cb1eac98740cd6
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-02-18 11:50:03 +0100

    Time: 0 ms (100.00%), Space: 39.7 MB (72.42%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -13,18 +13,37 @@
  *     }
  * }
  */
+/*
+Understanding:
+    1->2->4
+    |  |
+    |  8
+    |
+    5->5
+    |
+    5
+        result:root=1
+    Questions:
+        What do you mean by invert?left side became right side.
+        What is the range of the node number?[0,100]
+        What is the range of its values?[-100,100]
+Target:
+    return the root of the giving tree
+Solution:
+
+*/
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        invert(root);
-        return root;
+        reverseSides(root);
+       return root;
     }
-    private void invert(TreeNode root){
-        if(root==null) return;
-        invert(root.left);
-        invert(root.right);
-        TreeNode cup=root.left;
+    public void reverseSides(TreeNode root){
+        if(root==null)
+            return;
+        TreeNode tmp=root.left;
         root.left=root.right;
-        root.right=cup;
-        return;
+        root.right=tmp;
+        reverseSides(root.left);
+        reverseSides(root.right);
     }
 }
