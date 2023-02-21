3754b5f32f3b018dd8dd380994afea4e3bf80d45
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-02-21 21:49:31 +0100

    Time: 3 ms (10.17%), Space: 47.2 MB (5.65%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -14,25 +14,21 @@
  * }
  */
 class Solution {
-  public int kthSmallest(TreeNode root, int k) {
-      Stack<TreeNode> st = new Stack<>();
-      
-      while (root != null) {
-          st.push(root);
-          root = root.left;
-      }
-          
-      while (k != 0) {
-          TreeNode n = st.pop();
-          k--;
-          if (k == 0) return n.val;
-          TreeNode right = n.right;
-          while (right != null) {
-              st.push(right);
-              right = right.left;
-          }
-      }
-      
-      return -1; // never hit if k is valid
-}
+    public int kthSmallest(TreeNode root, int k) {
+        Queue<Integer> minHeap=new PriorityQueue<>();
+        trTreeToList(root,minHeap);
+        int ans=0;
+        while(k>0){
+            ans=minHeap.poll();
+            k--;
+        }
+        return ans;
+    }
+    public void trTreeToList(TreeNode root, Queue<Integer> minHeap){
+        if(root==null)
+            return;
+        minHeap.add(root.val);
+        trTreeToList(root.left,minHeap);
+        trTreeToList(root.right,minHeap);
+    }
 }
