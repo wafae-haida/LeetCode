bc9befd9bb9cb5e27667909db7fe96af46d78510
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-02-25 17:08:20 +0100

    Time: 26 ms (52.46%), Space: 42.5 MB (52.87%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -1,14 +1,33 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
 
-class Solution{
-    Map<Integer,Node> map=new HashMap<>();
-    Node cloneGraph(Node node){
-        if(node==null)return null;
-        if(map.containsKey(node.val))return map.get(node.val);
-        Node copy=new Node(node.val);
-        map.put(copy.val,copy);
-        for(Node nei:node.neighbors){
-            copy.neighbors.add(cloneGraph(nei));
-        }
-        return copy;
+class Solution {
+    Map<Integer,Node> cloned=new HashMap<>();
+    public Node cloneGraph(Node node) {
+        if(node==null)
+            return null;
+        if(cloned.containsKey(node.val)) return cloned.get(node.val);
+        Node clone=new Node(node.val);
+        cloned.put(node.val,clone);
+        for(Node neighbor:node.neighbors)
+            clone.neighbors.add(cloneGraph(neighbor));
+        return cloned.get(node.val);
     }
 }
