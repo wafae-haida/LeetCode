0aa405f78677f8ab30b3cf55d73abed70e650bd4
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-02-26 19:34:08 +0100

    Time: 3 ms (89.81%), Space: 42.5 MB (72.82%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,33 +1,26 @@
-/*
-   seeck out for bi in the rows and so on 
-   if bi is not in prerequisites[i] return false
-   
-
-
-*/
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites){
-        List<List<Integer>> adjs= new ArrayList<>();
-        for(int i=0;i<numCourses;i++)adjs.add(new ArrayList<>());
-        for(int i=0;i<prerequisites.length;i++)adjs.get(prerequisites[i][0]).add(prerequisites[i][1]);
-        int[] visited = new int[numCourses];
-        for(int i=0;i<numCourses;i++){
-            if(visited[i]==0){
-                if(isCycle(adjs,visited,i)) return false;
-            }
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj=new ArrayList<>(numCourses);
+        for(int i=0;i<numCourses;i++)
+            adj.add(new ArrayList<Integer>());
+        int[] visited=new int[numCourses];
+        for(int[] pre:prerequisites)
+             adj.get(pre[0]).add(pre[1]);
+        for(int i=0;i<adj.size();i++){
+            if(visited[i]==0 && isCycle(adj,visited,i))
+                return false;
         }
-        return true;
+        return true;    
     }
-    public boolean isCycle(List<List<Integer>> adjs,int[] visited, int curr){
-        if(visited[curr]==2)return true;
-        visited[curr]=2;
-        for(int i=0;i<adjs.get(curr).size();i++){
-            if(visited[adjs.get(curr).get(i)]!=1){
-               if(isCycle(adjs,visited,adjs.get(curr).get(i))) return true;
-            }
+    public boolean isCycle(List<List<Integer>> adj, int[] visited, int course){
+        if(visited[course]==2)
+            return true;
+        visited[course]=2;
+        for(int i=0;i<adj.get(course).size();i++){
+                if(visited[adj.get(course).get(i)]!=1 && isCycle(adj,visited,adj.get(course).get(i)))
+                return true;
         }
-        visited[curr]=1;
+        visited[course]=1;
         return false;
     }
-   
 }
