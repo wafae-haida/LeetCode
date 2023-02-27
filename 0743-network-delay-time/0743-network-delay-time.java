d364055860daa7a83c915855579f42bb6e834bbe
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-02-27 19:38:18 +0100

    Time: 17 ms (60.34%), Space: 47.1 MB (28.34%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,23 +1,26 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        int[] path=new int[n];
-        Arrays.fill(path,Integer.MAX_VALUE);
-        path[k-1]=0;
-        for(int j=0;j<n;j++){
-            for(int i=0;i<times.length;i++){
-                int src=times[i][0];
-                int trgt=times[i][1];
-                int time=times[i][2];
-                if(path[src-1]!=Integer.MAX_VALUE && path[src-1]+time<path[trgt-1])
-                    path[trgt-1]=path[src-1]+time;
-            }
+        int[] paths = new int[n];
+        Arrays.fill(paths,Integer.MAX_VALUE);
+        paths[k-1]=0;
+        for(int j = 0 ; j < n ; j++){
+             int[] tmp = Arrays.copyOf(paths , n);
+                for(int i = 0 ; i < times.length ; i++){
+                    int src = times[i][0];
+                    int target = times[i][1];
+                    int time = times[i][2];
+                    if(tmp[src - 1] != Integer.MAX_VALUE && tmp[src - 1] + time < tmp[target - 1])
+                        tmp[target - 1] = tmp[src - 1] + time;
+                }
+            paths = tmp;
         }
-        int ans=Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            if(path[i]==Integer.MAX_VALUE)return -1;
-                ans=Math.max(ans,path[i]);
+        
+        int rs = Integer.MIN_VALUE;
+        for(int i = 0 ; i < n ; i++){
+            if(paths[i] == Integer.MAX_VALUE)
+                return -1 ;
+            rs = Math.max(rs , paths[i]);
         }
-        return ans;
+        return rs;
     }
-   
 }
