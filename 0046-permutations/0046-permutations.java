4f0ae28e182692c999de9a525a8ec15f6c6726e3
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-02-28 16:20:01 +0100

    Time: 1 ms (90.67%), Space: 42.8 MB (34.30%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,18 +1,27 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();
-        backtrack(nums,ans,new ArrayList<Integer>(),0);
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack(nums, 0, ans);
         return ans;
     }
-    public void backtrack(int[] nums,List<List<Integer>> ans,List<Integer> cur,int start){
-        if(cur.size()==nums.length){
-            ans.add(new ArrayList<>(cur));
+    public void backtrack(int[] nums, int start, List<List<Integer>> ans){
+        if(start == nums.length){
+            List<Integer> list = new ArrayList<>();
+            for(int num : nums)
+                list.add(num);
+            ans.add(new ArrayList<Integer>(list));
             return;
         }
-        for(int i=0;i<=cur.size();i++){
-            cur.add(i,nums[start]);
-            backtrack(nums,ans,cur,start+1);
-            cur.remove(i);
+
+        for(int i = start ; i < nums.length ; i++){
+            swap(nums, start, i);
+            backtrack(nums, start + 1, ans);
+            swap(nums, start, i);
         }
+    }
+    public void swap(int[] nums, int start, int i){
+        int tmp = nums[start];
+        nums[start] = nums[i];
+        nums[i] = tmp;
     }
 }
