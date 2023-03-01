b4d2080eab780f1043b393aee8f49cc28875db66
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-01 16:14:30 +0100

    Time: 1 ms (100.00%), Space: 43.3 MB (34.95%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,41 +1,26 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        
-        List<List<Integer>> ans = new ArrayList<>(); // ans list
-        Arrays.sort(nums); // sort the array
-		
-		// create a boolean array which track the certain indexed integer is present in out temp list or not
-        helper(ans, new ArrayList<>(), nums, new boolean[nums.length]); // helper method
-		
-        return ans; // return output
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(nums,new ArrayList<>(), new boolean[nums.length], ans);
+        return ans;
     }
-  private void helper(List<List<Integer>> ans, List<Integer> temp, int[] nums, boolean[] vis) {
-		
-		/*
-			when size of the temp list and size of the inout array are same then add this temp list into the final ans list.
-			this is one of our permutation for given integer array
-		*/
-        if(nums.length == temp.size()) {
-            ans.add(new ArrayList<>(temp));
+    public void  backtrack(int[] nums,List<Integer> list, boolean[] visited,List<List<Integer>> ans){
+        if(list.size() == nums.length) {
+            ans.add(new ArrayList<>(list));
             return;
         }
-		
-        /*
-			Traverse for whole array one by one.
-		*/
-        for(int i=0;i<nums.length; i++) {
-		
-            if(vis[i]) continue; // if current element is already present in the temp, skip the element
-            if(i > 0 && ! vis[i-1] && nums[i] == nums[i-1]) continue; // if prev element and current element is equal, skip the element
+        for(int i = 0 ; i < nums.length ; i++){
+            if(visited[i])
+                continue;
+            if(i != 0 && !visited[i-1] && nums[i] == nums[i-1])
+                continue;
             
-            vis[i] = true;
-            temp.add(nums[i]);
-            
-            helper(ans, temp, nums, vis); // recursive call
-            
-			// backtracking
-            temp.remove(temp.size() - 1);
-            vis[i] = false;
+            list.add(nums[i]);
+            visited[i] = true;
+            backtrack(nums, list, visited, ans);
+            visited[i] = false;
+            list.remove(list.size() - 1);
         }
     }
 }
