5ea69bd3731ea79a411d4c25b7389b06e1e0c924
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-04 12:32:16 +0100

    Time: 23 ms (42.54%), Space: 44.4 MB (12.36%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,35 +1,14 @@
-/*
-    uppercase->lowercase + remove all non-alphanumeric characters 
-    reading forward is the same as backward
-    alphanumeric(digits & english letters)
-        "example258"
-        "1"
-        "M"
-        "ex 1 xE"
-         i-> <-j is it [a,z] Or [0,9] because we are taking character by character 
-        "3pap3"
-    target return true or false
-    two pionters method
-    how to define that it is a digit
-
-*/
 class Solution {
     public boolean isPalindrome(String s) {
-      int i=0,j=s.length()-1;
-        while(i<=j){
-            Character start=s.charAt(i);
-            Character end=s.charAt(j);
-            if(!Character.isLetterOrDigit(start)){
-                i++;
-                continue;
-            }
-            if(!Character.isLetterOrDigit(end)){
-                j--;
-                continue;
-            }
-            if(Character.toLowerCase(start)!=Character.toLowerCase(end))return false;
-            j--;
-            i++;
+        s = s.toLowerCase();
+        s = s.replaceAll("[^a-z0-9]", "").replaceAll("\\s","");
+        int left = 0, right = s.length()-1;
+        System.out.print(s);
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right))
+                return false;
+            left++;
+            right--;
         }
         return true;
     }
