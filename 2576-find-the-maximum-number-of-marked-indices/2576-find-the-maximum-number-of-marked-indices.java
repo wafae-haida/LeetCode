e8779053488241355a552de15a0c568253d31160
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-04 14:09:12 +0100

    Time: 29 ms (99.57%), Space: 61.6 MB (23.12%) - LeetHub

--- 2576-find-the-maximum-number-of-marked-indices/2576-find-the-maximum-number-of-marked-indices.java
+++ 2576-find-the-maximum-number-of-marked-indices/2576-find-the-maximum-number-of-marked-indices.java
@@ -1,11 +1,11 @@
 class Solution {
     public int maxNumOfMarkedIndices(int[] nums) {
-        int count = 0, j = nums.length-1;
         Arrays.sort(nums);
-        for(int i=nums.length/2-1; i>=0; i--){
-            if(nums[i] <= nums[j]/2){
+        int count = 0, right = nums.length - 1;
+        for(int left = nums.length/2 - 1; left >= 0 ; left--){
+            if(nums[left] * 2 <= nums[right]){
                 count += 2;
-                j--;
+                right--;
             }
         }
         return count;
