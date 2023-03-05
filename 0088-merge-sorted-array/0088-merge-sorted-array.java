2b35d49b41ac714456d8f0bec5de1f26a9b349ce
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-05 23:20:00 +0100

    Time: 0 ms (100.00%), Space: 42.4 MB (54.63%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -15,11 +15,11 @@ class Solution {
             }
             k--;
         }
-        
-        while(j>=0){
+         while(j>=0){
             nums1[k] = nums2[j];
             j--; k--;
         }
+       
     
     }
 }
