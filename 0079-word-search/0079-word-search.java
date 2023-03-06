3a206377ee918d237253c8ecf5e2305b3ff9d89c
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-06 14:53:23 +0100

    Time: 190 ms (70.36%), Space: 41 MB (36.68%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,26 +1,26 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
         for(int r = 0 ; r < board.length ; r++){
-             for(int c = 0 ; c < board[r].length ; c++){
-                if(backtrack(board, word, 0, r, c))
-                    return true;
-             }
-         }
+            for(int c = 0 ; c < board[r].length ; c++){
+                if(word.charAt(0) == board[r][c]){
+                    if(backtrack(board, word, r, c, 0))
+                        return true;
+                }
+            }
+        }
         return false;
     }
-    public boolean backtrack(char[][] board, String word, int indx, int r, int c){
-        if(indx == word.length())
+    public boolean backtrack(char[][] board, String word, int r, int c, int idx){
+        if(idx == word.length())
             return true;
-        if(r < 0 || c < 0 || r == board.length || c == board[r].length || board[r][c] != word.charAt(indx))
+        if(r < 0 || r == board.length || c < 0 || c == board[r].length || board[r][c] != word.charAt(idx))
             return false;
         boolean exist = false;
-        if(board[r][c] == word.charAt(indx))
-            board[r][c] += 20;
-        exist = backtrack(board, word, indx + 1, r + 1, c)
-        || backtrack(board, word, indx + 1, r - 1, c)
-        || backtrack(board, word, indx + 1, r, c + 1)
-        || backtrack(board, word, indx + 1, r, c - 1);
-        board[r][c] -= 20;
+        if(board[r][c] == word.charAt(idx))
+            board[r][c] += 100;
+        exist = backtrack(board, word, r - 1, c, idx + 1) || backtrack(board, word, r + 1, c, idx + 1) ||
+            backtrack(board, word, r, c - 1, idx + 1) || backtrack(board, word, r, c + 1, idx + 1);
+        board[r][c] -= 100;
         return exist;
     }
 }
