c6e8e73053bee85389800a3f5ff79cfcb5d10a95
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-06 21:09:38 +0100

    Time: 1 ms (44.50%), Space: 42.3 MB (13.88%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
@@ -1,22 +1,21 @@
 class Solution {
     public boolean isAlienSorted(String[] words, String order) {
-        Map<Character,Integer> map=new HashMap<>();
-        int c=0;
-        for(char ch:order.toCharArray()){
-            map.put(ch,c++);
-        }
-          for(int i = 0; i < words.length - 1; i++) {
-            String w1 = words[i], w2 = words[i + 1];
-            
-            for(int j = 0; j < w1.length(); j++)
+        Map<Character, Integer> dic = new HashMap<>();
+        int idx = 0;
+        for(char c : order.toCharArray())
+            dic.put(c,idx++);
+        for(int i = 0 ; i < words.length - 1 ; i++){
+            String w1 = words[i];
+            String w2 = words[i + 1];
+            for(int j = 0 ; j < w1.length(); j++){
                 if(j == w2.length())
                     return false;
-                else if(w1.charAt(j) != w2.charAt(j))
-                    if(map.get(w2.charAt(j)) < map.get(w1.charAt(j)))
-                        return false;
-                    else
-                        break;
+                if(dic.get(w1.charAt(j)) > dic.get(w2.charAt(j)))
+                   return false;
+                if(dic.get(w1.charAt(j)) < dic.get(w2.charAt(j)))
+                   break;
+            }
         }
-        return true;
+         return true;          
     }
 }
