2a3463c3b0b0ccadc9ad4a6f84abfb248954f313
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-06 23:39:26 +0100

    Time: 0 ms (100.00%), Space: 42.6 MB (11.53%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -14,24 +14,16 @@
  * }
  */
 class Solution {
-    public TreeNode flattenHelper(TreeNode root, TreeNode prev) {
-        // base case
-        if (root == null)
-            return prev;
-        
-        // hypothesis step
-        TreeNode right = flattenHelper(root.right, prev);
-		// for left subtree prev would be the node that we get from the right subtree recursion
-        TreeNode left = flattenHelper(root.left, right);
-        
-        // induction step
+    public void flatten(TreeNode root) {
+        dfs(root,null);
+    }
+    public TreeNode dfs(TreeNode root, TreeNode pre){
+        if(root == null)
+            return pre;
+        TreeNode right = dfs(root.right,pre);
+        TreeNode left = dfs(root.left,right);
         root.right = left;
         root.left = null;
-        
         return root;
-    }
-    
-    public void flatten(TreeNode root) {
-        flattenHelper(root, null);
     }
 }
