6954e8a966aa2a0080f722d69ae296810083bf33
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-07 20:28:24 +0100

    Time: 1 ms (99.68%), Space: 41.2 MB (21.67%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -2,7 +2,7 @@ class Solution {
     public char findTheDifference(String s, String t) {
         char c = 0;
         for(char n : t.toCharArray())
-            c ^=n;
+              c ^=n;
         for(char n : s.toCharArray())
             c ^=n;
         return c;
