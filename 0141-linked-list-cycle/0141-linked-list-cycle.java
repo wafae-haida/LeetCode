f5bf574a5f5aac1520c2d3504b67a4384d8b082d
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-07 21:46:11 +0100

    Time: 4 ms (18.08%), Space: 43.2 MB (88.93%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,30 +9,14 @@
  *     }
  * }
  */
-/*
-Understanding:
-    1->3->10->5->0->1
-    1->3->10->5->0->5->10 is it possible
-    1->3->10->5->0->3
-    empty
-    1->2->1 do we consider this of length 2?
-    Questions:
-        What is the range length of the linked list?start with 0.
-        What is the values range of the linked list?we do have negative value.
-Target:
-    return true if there is a cycle and false otherways.
-
-Solution:
-    I am thinking of using a set that refer to the visited nodes
-
-*/
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        Set<ListNode> visited=new HashSet<>();
-        ListNode cur=head;
-        while(cur!=null){
-            if(!visited.add(cur)) return true;
-            cur=cur.next;
+        Set<ListNode> visited = new HashSet<>();
+        ListNode cur = head;
+        while(cur != null){
+            if(!visited.add(cur))
+                return true;
+            cur = cur.next;
         }
         return false;
     }
