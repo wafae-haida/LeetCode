3b7f020439bde9aac81c8374d72b156b0236e6df
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-07 17:44:53 +0100

    Time: 1 ms (65.91%), Space: 43.1 MB (23.93%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,23 +1,24 @@
 class Solution {
     int[] parent;
     public int[] findRedundantConnection(int[][] edges) {
-        parent = new int[edges.length];
-        for(int i=0;i<edges.length;i++)
-            parent[i]=i+1;
-        
-        for(int[] edge : edges){
+        int n = edges.length;
+        parent = new int[n];
+        for(int i = 0 ; i < n; i++)
+            parent[i] = i+1;
+        for(int[] edge :edges){
             if(findParent(edge[0]) == findParent(edge[1]))
                 return edge;
             union(edge[0],edge[1]);
         }
-        return new int[2];
+        return new int[]{};
     }
-    public int findParent(int node){
-        if(node==parent[node-1])
-            return node; 
-        return findParent(parent[node-1]);
+    public int findParent(int vertix){
+        if(vertix == parent[vertix-1])
+            return vertix;
+        return findParent(parent[vertix-1]);
     }
-    public void union(int firstNode,int secondNode){
-        parent[findParent(firstNode)-1] = findParent(secondNode);
+    public void union(int first,int second){
+        parent[findParent(first) - 1] = findParent(second);
     }
+    
 }
