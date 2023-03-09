102ec4468f49a899a8c9dc2ea9b74ddf7453a126
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-09 23:08:51 +0100

    Time: 2 ms (33.13%), Space: 42.7 MB (34.69%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,22 +1,20 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        backtrack(nums, 0, ans);
+        dfs(nums, 0, ans);
         return ans;
     }
-    public void backtrack(int[] nums, int start, List<List<Integer>> ans){
+    public void dfs(int[] nums, int start,List<List<Integer>> ans){
         if(start == nums.length){
             List<Integer> list = new ArrayList<>();
             for(int num : nums)
                 list.add(num);
-            ans.add(new ArrayList<Integer>(list));
-            return;
+            ans.add(new ArrayList<>(list));
         }
-
-        for(int i = start ; i < nums.length ; i++){
-            swap(nums, start, i);
-            backtrack(nums, start + 1, ans);
-            swap(nums, start, i);
+        for(int i = start ; i < nums.length; i++){
+            swap(nums,start,i);
+            dfs(nums, start + 1, ans);
+            swap(nums,start,i);
         }
     }
     public void swap(int[] nums, int start, int i){
