6fff6021d5cc9354fd6d57a00c9ef7b7f02db7e4
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-10 20:06:17 +0100

    Time: 1 ms (77.83%), Space: 41.5 MB (15.45%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -14,34 +14,29 @@
  * }
  */
 class Solution {
-   public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-   TreeNode c=root;
-   List<List<Integer>> ans =new ArrayList<List<Integer>>();
-   if(c==null) return ans;
-   Stack<TreeNode> s1=new Stack<TreeNode>();
-   Stack<TreeNode> s2=new Stack<TreeNode>();
-   s1.push(root);
-   while(!s1.isEmpty()||!s2.isEmpty())
-   {
-       List<Integer> tmp=new ArrayList<Integer>();
-        while(!s1.isEmpty())
-        {
-            c=s1.pop();
-            tmp.add(c.val);
-            if(c.left!=null) s2.push(c.left);
-            if(c.right!=null) s2.push(c.right);
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        if(root == null)
+            return new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        Deque<TreeNode> deque = new ArrayDeque<>();
+        deque.add(root);
+        boolean reverse = false;
+        while(!deque.isEmpty()){
+            int len = deque.size();
+            List<Integer> lst = new ArrayList<>();
+            for(int i = 0 ; i < len; i++){
+                TreeNode node = deque.pollFirst();
+                lst.add(node.val);
+                if(node.left != null)
+                    deque.addLast(node.left);
+                if(node.right != null)
+                    deque.addLast(node.right);
+            }
+            if(reverse)
+                Collections.reverse(lst);
+            res.add(new ArrayList<>(lst));
+            reverse = !reverse;
         }
-        ans.add(tmp);
-        tmp=new ArrayList<Integer>();
-        while(!s2.isEmpty())
-        {
-            c=s2.pop();
-            tmp.add(c.val);
-            if(c.right!=null)s1.push(c.right);
-            if(c.left!=null)s1.push(c.left);
-        }
-        if(!tmp.isEmpty()) ans.add(tmp);
-   }
-   return ans;
-}
+        return res;
+    }
 }
