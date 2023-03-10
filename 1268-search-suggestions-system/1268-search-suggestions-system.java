2531b2f287d4e75d9114bbc8405d06ee23e726b6
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-10 21:10:54 +0100

    Time: 173 ms (14.59%), Space: 51.4 MB (12.01%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -0,0 +1,57 @@
+class Trie{
+    Trie[] children;
+    boolean end;
+    public Trie(){
+        children = new Trie[26];
+        end = false;
+    }
+    public void add_Product(String product){
+        Trie cur = this;
+        for(char c : product.toCharArray()){
+            if(cur.children[c-'a']==null)
+                cur.children[c-'a'] = new Trie();
+            cur = cur.children[c-'a'];
+        }
+        cur.end = true;
+    }
+    public List<String> search_Word(String searchWord){
+        List<String> lst = new ArrayList<>();
+        Trie cur = this;
+        for(char c : searchWord.toCharArray()){
+            if(cur.children[c - 'a'] != null){
+                cur = cur.children[c - 'a'];
+            } else {
+                return new ArrayList<>();
+            }
+        }
+        traverse(cur, searchWord, lst);
+        return lst;
+    }
+    private void traverse(Trie cur, String searchWord, List<String> lst){
+        if(cur.end){
+            lst.add(searchWord);
+            if(lst.size() == 3) return;
+        }
+        for(char c = 'a'; c <= 'z' && lst.size() < 3; c++){
+            if(cur.children[c - 'a'] != null){
+                traverse(cur.children[c - 'a'], searchWord + c, lst);
+            }
+        }
+    }
+}
+
+class Solution {
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        Trie tr = new Trie();
+        List<List<String>> ans = new ArrayList<>();
+        for(String product : products)
+            tr.add_Product(product);
+        String sch = "";
+        for(char c :searchWord.toCharArray()){
+            sch += c;
+            List<String> lst = tr.search_Word(sch); // corrected line
+            ans.add(lst);
+        }
+        return ans;
+    }
+}
