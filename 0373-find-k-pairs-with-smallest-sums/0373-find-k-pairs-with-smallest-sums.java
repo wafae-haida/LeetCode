0437b8a2e9d9aca1420de72e4423819d400832db
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-12 18:08:20 +0100

    Time: 33 ms (69.66%), Space: 59.6 MB (43.10%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -1,40 +1,22 @@
 class Solution {
-   class pair implements Comparable<pair>{
-    int num1;
-    int num2;
-    int sum;
-    int idx;
-    pair(int num1,int num2,int sum,int idx){
-        this.num1=num1;
-        this.num2=num2;
-        this.sum=sum;
-        this.idx=idx;
-    }
-    public int compareTo(pair p){
-        return this.sum-p.sum;
-    }
-}
-
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        PriorityQueue<pair>pq=new PriorityQueue<>();
-
-        for(int i=0;i<Math.min(k,nums1.length);i++){
-                pq.add(new pair(nums1[i],nums2[0],nums1[i]+nums2[0],0));
-        }
-
-        List<List<Integer>>list=new ArrayList<>();
         
-        while(k-->0 && pq.size()>0){
-            List<Integer>l=new ArrayList<>();
-            pair p=pq.poll();
-            l.add(p.num1);
-            l.add(p.num2);
-            list.add(l);
-            int idx=p.idx;
-            if(idx<nums2.length-1){
-                pq.add(new pair(p.num1,nums2[idx+1],p.num1+nums2[idx+1],p.idx+1));
-            }
+		//In the minHeap array, 0th element refers to the curr element in nums1 and 1st element refers to curr element in nums2 
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> (a[0] + a[1]) - (b[0] + b[1]));
+        
+		// The 2nd element in the minHeap is the index of nums2, the corresponding element of nums2 is in index1 of minHeap
+        for(int i=0; i < nums1.length && i < k; i++)
+            minHeap.add(new int[]{nums1[i], nums2[0], 0});
+        
+        List<List<Integer>> result = new ArrayList<>();
+        
+        for(int i=0; i < k && !minHeap.isEmpty(); i++){
+            int[] curr = minHeap.poll();
+            result.add(List.of(curr[0], curr[1]));
+            int nums2Idx = curr[2];
+            if(nums2Idx < nums2.length - 1)
+                minHeap.add(new int[]{curr[0], nums2[nums2Idx + 1], nums2Idx + 1});
         }
-        return list;
-        }
+        return result;
+    }
 }
