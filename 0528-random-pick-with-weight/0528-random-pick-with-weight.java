63ccb3219a9c3c4161e6f82dd251465b0344908c
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-12 21:35:03 +0100

    Time: 25 ms (95.85%), Space: 46.7 MB (82.65%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.java
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.java
@@ -1,25 +1,28 @@
 class Solution {
-
-    private int[] idx;
-
+    int[] idx;
     public Solution(int[] w) {
-        for (int i = 1; i < w.length; i++)
-        w[i] += w[i - 1];
+        
+        for(int i = 1 ; i < w.length; i++)
+            w[i] += w[i-1];
+        
         double sum = w[w.length - 1];
-        double[] dw = new double[w.length];
-        for (int i = 0; i < w.length; i++)
-        dw[i] = w[i] * 1000 / sum;
+        double[] pb = new double[w.length]; 
+        
+        for(int i = 0 ; i < w.length; i++)
+            pb[i] = w[i]*1000/sum;
+        
         idx = new int[1000];
         int k = 0;
-        for (int i = 0; i < w.length; i++)
-        while (k < dw[i])
-        idx[k++] = i;
+        for(int i = 0 ; i < w.length; i++)
+            while(k<pb[i])
+                idx[k++] = i;
     }
-
+    
     public int pickIndex() {
         return idx[(int) (Math.random() * 1000)];
     }
 }
+
 /**
  * Your Solution object will be instantiated and called as such:
  * Solution obj = new Solution(w);
