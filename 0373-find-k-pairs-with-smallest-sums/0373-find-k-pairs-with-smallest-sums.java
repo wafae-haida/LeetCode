8b3aefcc82bb331032cc235865ef9c9e1ab01c32
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-12 17:52:26 +0100

    Time: 31 ms (78.65%), Space: 59.1 MB (45.96%) - LeetHub

---
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -0,0 +1,38 @@
+class Solution {
+   class pair implements Comparable<pair>{
+    int num1;
+    int num2;
+    int sum;
+    int idx;
+    pair(int num1,int num2,int sum,int idx){
+        this.num1=num1;
+        this.num2=num2;
+        this.sum=sum;
+        this.idx=idx;
+    }
+    public int compareTo(pair p){
+        return this.sum-p.sum;
+    }
+}
+
+public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+    PriorityQueue<pair>pq=new PriorityQueue<>();
+    for(int i=0;i<Math.min(k,nums1.length);i++){
+            pq.add(new pair(nums1[i],nums2[0],nums1[i]+nums2[0],0));
+    }
+    
+    List<List<Integer>>list=new ArrayList<>();
+    while(k-->0 && pq.size()>0){
+        List<Integer>l=new ArrayList<>();
+        pair p=pq.remove();
+        l.add(p.num1);
+        l.add(p.num2);
+        list.add(l);
+        int idx=p.idx;
+        if(idx<nums2.length-1){
+            pq.add(new pair(p.num1,nums2[idx+1],p.num1+nums2[idx+1],p.idx+1));
+        }
+    }
+    return list;
+    }
+}
