3a1acf3571d7fb3c2b7253fc76656a21312c4c24
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-13 16:50:37 +0100

    Time: 32 ms (82.15%), Space: 51.8 MB (38.39%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,30 +1,21 @@
 class Solution {
-    Boolean mem[][];
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        int n = nums.length;
-        
-        for(int i : nums) sum+=i;
-        
-        if(sum%2!=0) return false;
-        
-        sum /= 2;
-        
-        mem = new Boolean[n+1][sum+1];
-        
-        return subsetSum(nums,0,sum);
+        for(int num : nums)
+            sum += num;
+        if(sum % 2 != 0)
+            return false;
+        sum /=2;
+        Boolean[][] dp = new Boolean[nums.length][sum + 1];
+        return backtrack(nums, sum, 0, dp);
     }
-    
-    boolean subsetSum(int[] nums, int pos, int sum){
-        if(sum==0) return true;
-        
-        else if(pos>=nums.length || sum<0) return false;
-        
-        if(mem[pos][sum]!=null) return mem[pos][sum];
-        
-        return mem[pos][sum] = subsetSum(nums,pos+1,sum-nums[pos]) ||
-                                subsetSum(nums,pos+1,sum);
-        
-        
+    public boolean backtrack(int[] nums, int sum, int start, Boolean[][] dp){
+        if(sum == 0)
+            return true;
+        if(start >= nums.length || sum < 0)
+            return false;
+        if(dp[start][sum]!= null)
+            return dp[start][sum];
+        return dp[start][sum] = backtrack(nums, sum - nums[start], start + 1, dp) || backtrack(nums, sum, start + 1, dp);
     }
 }
