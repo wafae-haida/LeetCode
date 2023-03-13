23c7b8603750342d6b2f4a9ff99a715c874632aa
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-13 22:59:31 +0100

    Time: 3 ms (90.88%), Space: 43.1 MB (30.85%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,26 +1,28 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adj=new ArrayList<>(numCourses);
-        for(int i=0;i<numCourses;i++)
-            adj.add(new ArrayList<Integer>());
-        int[] visited=new int[numCourses];
-        for(int[] pre:prerequisites)
-             adj.get(pre[0]).add(pre[1]);
-        for(int i=0;i<adj.size();i++){
-            if(visited[i]==0 && isCycle(adj,visited,i))
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0 ; i < numCourses; i++)
+            adj.add(new ArrayList<>());
+        
+        int[] visited = new int[numCourses];
+        for(int i = 0 ; i < prerequisites.length ; i++)
+            adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        
+        for(int i = 0 ; i < numCourses; i++){
+            if(visited[i] == 0 && isCycle(visited,adj,i))
                 return false;
         }
-        return true;    
+       return true;
     }
-    public boolean isCycle(List<List<Integer>> adj, int[] visited, int course){
-        if(visited[course]==2)
+    public boolean isCycle(int[] visited, List<List<Integer>> adj, int course){
+        if(visited[course] == 2)
             return true;
-        visited[course]=2;
-        for(int i=0;i<adj.get(course).size();i++){
-                if(visited[adj.get(course).get(i)]!=1 && isCycle(adj,visited,adj.get(course).get(i)))
+        visited[course] = 2;
+        for(int i = 0 ; i< adj.get(course).size() ; i++){
+            if(visited[adj.get(course).get(i)] != 1 && isCycle(visited,adj,adj.get(course).get(i)))
                 return true;
         }
-        visited[course]=1;
+        visited[course] = 1;
         return false;
     }
 }
