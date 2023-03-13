f8edb3cfac434e3dde5eb4740317c003b510701e
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-13 17:48:51 +0100

    Time: 38 ms (40.21%), Space: 61.4 MB (54.74%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+        int second_max = Integer.MIN_VALUE;
+        for(int i = nums.length - 1 ; i >= 0; i--){
+            if(nums[i] < second_max)
+                return true;
+            while(!stack.isEmpty() && nums[i] > stack.peek()){
+                second_max = stack.pop();
+            }   
+           stack.push(nums[i]);
+        }
+        return false;
+    }
+}
