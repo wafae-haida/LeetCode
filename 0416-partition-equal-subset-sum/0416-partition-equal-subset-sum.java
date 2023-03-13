d9924038cfa79abcda72fff999f4e28188c99f87
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-13 16:13:43 +0100

    Time: 54 ms (58.99%), Space: 52.4 MB (35.75%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,26 +1,30 @@
 class Solution {
-
+    Boolean mem[][];
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for (int num : nums) {
-            sum += num;
-        }
-        if (sum % 2 != 0) return false;
-        int target = sum / 2;
-        boolean[][] dp = new boolean[nums.length + 1][target + 1];
         int n = nums.length;
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= target; j++) {
-                if (i == 0 || j == 0) {
-                    if (i == 0) dp[i][j] = false; else if (j == 0) dp[i][j] =
-                        true;
-                } else if (j >= nums[i - 1]) {
-                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
-        }
-        return dp[n][target];
+        
+        for(int i : nums) sum+=i;
+        
+        if(sum%2!=0) return false;
+        
+        sum /= 2;
+        
+        mem = new Boolean[n+1][sum+1];
+        
+        return subsetSum(nums,0,sum);
+    }
+    
+    boolean subsetSum(int[] nums, int pos, int sum){
+        if(sum==0) return true;
+        
+        else if(pos>=nums.length || sum<0) return false;
+        
+        if(mem[pos][sum]!=null) return mem[pos][sum];
+        
+        return mem[pos][sum] = subsetSum(nums,pos+1,sum-nums[pos]) ||
+                                subsetSum(nums,pos+1,sum);
+        
+        
     }
 }
