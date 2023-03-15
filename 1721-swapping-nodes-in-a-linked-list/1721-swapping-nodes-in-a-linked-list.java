be79b37bdf8ba86c9b711aeee3d2b1d2fbeb15b6
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-15 17:26:22 +0100

    Time: 2 ms (99.95%), Space: 58 MB (25.69%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -14,7 +14,7 @@ class Solution {
         ListNode slow = head;
         ListNode first = null;
         
-        for(int i = 0; i < k - 1; ++i)
+        for(int i = 1; i < k ; ++i)
             fast = fast.next;
             
         first = fast;
