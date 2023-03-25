a59b567bada36fe3c94498c0cf0b795a57bce83d
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-25 17:14:26 +0000

    Time: 19 ms (60.41%), Space: 43.4 MB (28.24%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,51 +1,72 @@
-/*
-Understanding the problem:
-    s="example",length=m;t="exmaple",length=n
-    s="eXamPle";t="example"
-    min s="a";t="b"
-        Questions:
-            What are the ranges of m and n?
-            Do s and t contains lowercase english letters or Uppercase or both? Both
-                If it contains both do e.g:'A'=='a'? I'll asume that 'A'!='a' 
-            Is there any digits or symboles? No
-Target:
-    find a minsubstring from s that contains all the characters of t including duplicates.
-    return a string.
-Solution:
-    Loop over t and check if its caracters are included in s then chrink the left side and check again and again until one of the t characters is missing the go to chrink from the right side until one of the t characters is missing then return the result.
-        Problem how i can check all caracters of t without increasing the time complexity?
-        
-*/
 class Solution {
-    public String minWindow(String s, String t) { 
-        Map<Character,Integer> dic=new HashMap<>();
-        int have=0;
-        String rs="";
-        int start=0;
-        int len=s.length()+1;
-        int substart=0;
-        for(char c:t.toCharArray())
-            dic.put(c,dic.getOrDefault(c,0)+1);
-        for(int i=0;i<s.length();i++){
-            char cur=s.charAt(i);
-            if(dic.containsKey(cur)){
-                dic.put(cur,dic.get(cur)-1);
-                if(dic.get(cur) == 0) 
-                    have++;
-            }
-            while(have==dic.size()){
-                if(len>i-start+1){
-                    len=i-start+1;
-                    substart=start;
-                }
-                char deleted=s.charAt(start++);
-                if(dic.containsKey(deleted)){
-                    if(dic.get(deleted)==0)
-                        have--;
-                    dic.put(deleted,dic.get(deleted)+1);
-                }
-            }   
+    public String minWindow(String s, String t) {
+
+        if (s.length() == 0 || t.length() == 0) {
+            return "";
         }
-        return len>s.length()?"":s.substring(substart,substart+len);
-    } 
+
+        // Dictionary which keeps a count of all the unique characters in t.
+        Map<Character, Integer> dictT = new HashMap<Character, Integer>();
+        for (int i = 0; i < t.length(); i++) {
+            int count = dictT.getOrDefault(t.charAt(i), 0);
+            dictT.put(t.charAt(i), count + 1);
+        }
+
+        // Number of unique characters in t, which need to be present in the desired window.
+        int required = dictT.size();
+
+        // Left and Right pointer
+        int l = 0, r = 0;
+
+        // formed is used to keep track of how many unique characters in t
+        // are present in the current window in its desired frequency.
+        // e.g. if t is "AABC" then the window must have two A's, one B and one C.
+        // Thus formed would be = 3 when all these conditions are met.
+        int formed = 0;
+
+        // Dictionary which keeps a count of all the unique characters in the current window.
+        Map<Character, Integer> windowCounts = new HashMap<Character, Integer>();
+
+        // ans list of the form (window length, left, right)
+        int[] ans = { -1, 0, 0 };
+
+        while (r < s.length()) {
+            // Add one character from the right to the window
+            char c = s.charAt(r);
+            int count = windowCounts.getOrDefault(c, 0);
+            windowCounts.put(c, count + 1);
+
+            // If the frequency of the current character added equals to the
+            // desired count in t then increment the formed count by 1.
+            if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
+                formed++;
+            }
+
+            // Try and contract the window till the point where it ceases to be 'desirable'.
+            while (l <= r && formed == required) {
+                c = s.charAt(l);
+                // Save the smallest window until now.
+                if (ans[0] == -1 || r - l + 1 < ans[0]) {
+                    ans[0] = r - l + 1;
+                    ans[1] = l;
+                    ans[2] = r;
+                }
+
+                // The character at the position pointed by the
+                // `Left` pointer is no longer a part of the window.
+                windowCounts.put(c, windowCounts.get(c) - 1);
+                if (dictT.containsKey(c) && windowCounts.get(c).intValue() < dictT.get(c).intValue()) {
+                    formed--;
+                }
+
+                // Move the left pointer ahead, this would help to look for a new window.
+                l++;
+            }
+
+            // Keep expanding the window once we are done contracting.
+            r++;
+        }
+
+        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
+    }
 }
