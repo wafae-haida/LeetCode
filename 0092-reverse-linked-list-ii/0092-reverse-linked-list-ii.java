352d2cf489150fe47573bb21536d7ba2c881a034
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-26 13:10:15 +0000

    Time: 0 ms (100.00%), Space: 40.2 MB (54.42%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -9,20 +9,89 @@
  * }
  */
 class Solution {
-    public ListNode reverseBetween(ListNode head, int left, int right) {
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode prev = dummy;
-        for(int i = 0 ; i < left - 1; i++)
-            prev = prev.next;
-        ListNode cur = prev.next;
-        for(int i = 0; i < right - left;i++){
-            ListNode forw = cur.next;
-            cur.next = cur.next.next;
-            forw.next = prev.next;
-            prev.next = forw;
-            
+       public ListNode reverse(ListNode head, int left, int right) {
+        ListNode revHead = null;
+        ListNode ptr = head; // a pointer to traverse the original list.
+        while (right >= left) {
+            // Track the next node to traverse in the original list
+            ListNode next = ptr.next;
+
+            // At the beginning of the reversed list,
+            // insert the node pointed to by `ptr`
+            ptr.next = revHead;
+            revHead = ptr;
+
+            // Move on to the next node
+            ptr = next;
+
+            // Decrement the count of nodes to be reversed by 1
+            right -= 1;
         }
-        return dummy.next;
+
+        // Return reversed list's head
+        return revHead;
+    }
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode ptr = head; // a pointer to traverse the original list.
+        // a pointer to keep the track of previous node
+        ListNode nextNode = null;
+        ListNode previous = null;
+        ListNode reverseHead = null;
+        ListNode rightNode = null;
+        // Keep traversing until left and right node number
+        int count = 1;
+        // Move the ptr to the left number node
+        while (count < left && ptr != null){
+            previous = ptr; // keep track of the previous node
+            ptr = ptr.next;
+            count += 1;
+        }
+        
+        if (ptr != null) {
+            // keep track of the next node outside the [left - right] 
+            // interval
+            nextNode = ptr;
+            while (count <= right && nextNode != null) {
+                // keep track of the right number node
+                rightNode = nextNode;
+                nextNode = nextNode.next;
+                count += 1;
+            }
+            // If we have found the left till right nodes, then we 
+            // reverse them.
+            if (rightNode != null) {
+                // Reverse these [left-right] nodes and get the new head
+                //  of the reversed list
+                reverseHead = reverse(ptr, left, right);
+            }
+
+            if (previous != null) {
+                // point previous.next to the reversed linked list
+                previous.next = reverseHead;
+            }
+
+            if (nextNode != null) {
+                // traverse in the reversed linked list until the last node
+                ListNode tmp = reverseHead;
+                while (tmp.next != null) {
+                    tmp = tmp.next;
+                }
+                // add the next node to the reversed linked list
+                tmp.next = nextNode;
+            }
+
+        }
+
+
+        // We will reverse head if there are node before the [left-right]
+        // position interval
+        if (previous != null)
+            return head;
+        // We will simply return the reverse head if there is no node
+        // before the [left-right] position interval
+        else
+            return reverseHead;
+
     }
 }
