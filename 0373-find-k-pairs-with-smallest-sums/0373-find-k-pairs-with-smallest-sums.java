46a2d2a413d45399b926e7d74a860991e3f1ef3c
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-27 13:33:59 +0000

    Time: 32 ms (76.79%), Space: 62.6 MB (11.28%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -4,14 +4,15 @@ class Solution {
         
         for(int i = 0 ; i < nums1.length && minHeap.size() <= k ; i++)
             minHeap.add(new int[]{nums1[i],nums2[0],0});
+        
         List<List<Integer>> ans = new ArrayList<>();
+        
         while(k > 0 && !minHeap.isEmpty()){
             int[] tmp = minHeap.poll();
             ans.add(List.of(tmp[0],tmp[1]));
             int idx = tmp[2] + 1;
-            if(idx < nums2.length){
+            if(idx < nums2.length)
                 minHeap.add(new int[]{tmp[0],nums2[idx],idx});
-            }
             k--;
         }
         return ans;
