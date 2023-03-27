0c1fc0fa5bba11ee52a5e02644c1d3e2f46d558a
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-27 17:17:38 +0000

    Time: 1 ms (92.21%), Space: 42.5 MB (48.40%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -4,21 +4,20 @@ class Solution {
         backtrack(n, 0, 0, res, new StringBuilder());
         return res;
     }
-    void backtrack(int n, int open, int close, List<String> res, StringBuilder strSoFar){
-        if(strSoFar.length() == n*2){
-            res.add(strSoFar.toString());
+    void backtrack(int n, int open, int close, List<String> res, StringBuilder cur){
+        if(cur.length() == n*2){
+            res.add(cur.toString());
             return;
         }
         if(open<n){
-            strSoFar.append("(");
-            backtrack(n, open+1, close, res, strSoFar);
-            strSoFar.deleteCharAt(strSoFar.length()-1);
+            cur.append("(");
+            backtrack(n, open + 1, close, res, cur);
+            cur.deleteCharAt(cur.length() - 1);
         }
         if(close < open){
-            strSoFar.append(")");
-            backtrack(n, open, close+1, res, strSoFar);
-            strSoFar.deleteCharAt(strSoFar.length()-1);
+            cur.append(")");
+            backtrack(n, open, close + 1, res, cur);
+            cur.deleteCharAt(cur.length() - 1);
         }
-        System.out.println(strSoFar);
     }
 }
