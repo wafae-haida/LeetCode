09b20695a6fbbe29a925c8d0f8ea9f730a5731bd
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-27 16:54:07 +0000

    Time: 0 ms (100.00%), Space: 42.4 MB (42.33%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -3,25 +3,22 @@ class Solution {
         int left = 0, right = nums.length - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
+            if (nums[mid] == target)
                 return true;
-            }
             if (nums[left] == nums[mid]) {
                 left++;
                 continue;
             }
             if (nums[left] < nums[mid]) {
-                if (nums[left] <= target && target < nums[mid]) {
+                if (nums[left] <= target && target < nums[mid])
                     right = mid - 1;
-                } else {
+                else 
                     left = mid + 1;
-                }
             } else {
-                if (nums[mid] < target && target <= nums[right]) {
+                if (nums[mid] < target && target <= nums[right])
                     left = mid + 1;
-                } else {
+                else
                     right = mid - 1;
-                }
             }
         }
         return false;
