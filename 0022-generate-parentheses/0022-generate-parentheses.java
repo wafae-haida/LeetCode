517eadf8689f1bbbcf25c720c84d98e680ba7dde
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-27 17:15:23 +0000

    Time: 20 ms (5.27%), Space: 42.9 MB (18.66%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,11 +1,9 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        backtrack(n, 0, 0, res, sb);
+        backtrack(n, 0, 0, res, new StringBuilder());
         return res;
     }
-
     void backtrack(int n, int open, int close, List<String> res, StringBuilder strSoFar){
         if(strSoFar.length() == n*2){
             res.add(strSoFar.toString());
@@ -21,5 +19,6 @@ class Solution {
             backtrack(n, open, close+1, res, strSoFar);
             strSoFar.deleteCharAt(strSoFar.length()-1);
         }
+        System.out.println(strSoFar);
     }
 }
