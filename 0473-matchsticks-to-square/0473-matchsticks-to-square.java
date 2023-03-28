c792bc0e89e79cc2ee860e1418f560fdeb032be2
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-28 11:43:21 +0000

    Time: 161 ms (50.87%), Space: 40.7 MB (27.96%) - LeetHub

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.java
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.java
@@ -1,27 +1,24 @@
 class Solution {
     public boolean makesquare(int[] matchsticks) {
-        if(matchsticks == null || matchsticks.length == 0)return false;
-        int sum =0;
-        for(int num:matchsticks)sum+=num;
-        
-        if(sum%4!=0)return false;
-        
+        if(matchsticks == null || matchsticks.length == 0)
+            return false;
+        int sum = Arrays.stream(matchsticks).sum();
+        if(sum % 4 != 0)
+            return false;
         Arrays.sort(matchsticks);
-        
-        return dfs(matchsticks,new int[4], matchsticks.length-1,sum/4);
+        return dfs(matchsticks,new int[4], matchsticks.length - 1,sum/4);
     }
     
     boolean dfs(int[] matchsticks, int sum[], int index,int target){
-        if(index <0 )return true;
-        
-        for(int i=0;i<4;i++){
-            
-            if((sum[i] + matchsticks[index]<=target)){
-            sum[i]+=matchsticks[index];
-            if(dfs(matchsticks,sum,index-1,target))return true;
-            sum[i]-= matchsticks[index];
-            }
-              
+        if(index < 0 )
+            return true;
+        for(int i = 0; i < 4 ; i++){
+            if((sum[i] + matchsticks[index] <= target)){
+                sum[i] += matchsticks[index];
+            if(dfs(matchsticks,sum,index - 1,target))
+                return true;
+            sum[i] -= matchsticks[index];
+            } 
         }
         return false;
     }
