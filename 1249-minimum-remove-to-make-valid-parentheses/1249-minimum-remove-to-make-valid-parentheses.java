198b5a92dfef11c01716a844ebdcf6d844275da3
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-28 16:47:42 +0000

    Time: 716 ms (10.69%), Space: 43.4 MB (38.24%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -5,22 +5,21 @@ class Solution {
             char c = s.charAt(i);
             if(Character.isAlphabetic(c))
                 continue;
-            
-            if(s.charAt(i) == '(')
+            if(c == '(')
                 stack.push(i);
             else{
-                if(!stack.isEmpty() && s.charAt(stack.peek()) == '(')
+                if(!stack.isEmpty() && s.charAt(stack.peek()) == '(' && s.charAt(i) == ')')
                     stack.pop();
                 else
-                    stack.push(i);
+                    stack.push(i); 
             }
         }
+        List<Integer> seen = new ArrayList<>(stack);
         StringBuilder sb = new StringBuilder();
-        List<Integer> set = new ArrayList<>(stack);
-        for(int i = 0 ; i < s.length() ; i++)
-            if(!set.contains(i))
+        for(int i = 0 ; i < s.length() ; i++){
+            if(!seen.contains(i))
                 sb.append(s.charAt(i));
-        return sb.toString();
+        }
+        return sb.toString();  
     }
 }
-       
