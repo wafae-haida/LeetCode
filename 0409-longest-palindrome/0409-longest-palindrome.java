89f7d3bad2000ad1f8d091f2a4240cc1d0fa0eb2
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-29 23:17:54 +0000

    Time: 7 ms (34.88%), Space: 42.3 MB (18.27%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        Set<Character> set = new HashSet<>();
+        for(int i = 0 ; i < s.length() ; i++){
+            if(set.contains(s.charAt(i)))
+                set.remove(s.charAt(i));
+            else
+                set.add(s.charAt(i));
+        }
+        if(set.size() <= 1)
+            return s.length();
+        return s.length() - set.size() + 1;
+    }
+
+}
