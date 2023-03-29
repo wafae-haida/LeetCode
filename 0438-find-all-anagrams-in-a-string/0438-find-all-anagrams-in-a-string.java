ee29f0f7d081385a243705b0d6d8c65cf49b1747
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-29 23:57:59 +0000

    Time: 68 ms (22.57%), Space: 44.1 MB (6.07%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,39 +1,29 @@
 class Solution {
-    public List<Integer> findAnagrams(String a, String b) {
-       		List<Integer> ans = new ArrayList<Integer> ();
+    public List<Integer> findAnagrams(String a, String dic) {
+       	List<Integer> ans = new ArrayList<Integer> ();
 		// If len of b is more than len of a, then there could be no anagram of b in a
-		if (b.length() > a.length()) return ans;
-		// Hash map for count of a
+		if (dic.length() > a.length()) 
+            return ans;
+        HashMap<Character, Integer> countDic = new HashMap<>();
 		HashMap<Character, Integer> countA = new HashMap<>();
-		// Hash map for count of b
-		HashMap<Character, Integer> countB = new HashMap<>();
-		// Storing frequency of characters for length of b
-		for (int i = 0; i<b.length(); i++) {
-			if (!countA.containsKey(a.charAt(i))) countA.put(a.charAt(i), 1);
-			else countA.put(a.charAt(i), countA.get(a.charAt(i)) + 1);
-
-			if (!countB.containsKey(b.charAt(i))) countB.put(b.charAt(i), 1);
-			else countB.put(b.charAt(i), countB.get(b.charAt(i)) + 1);
+		// Storing frequency of characters for length of dic
+		for (int i = 0; i < dic.length() ; i++) {
+	        countA.put(a.charAt(i), countA.getOrDefault(a.charAt(i), 0) + 1);
+			countDic.put(dic.charAt(i), countDic.getOrDefault(dic.charAt(i), 0) + 1);
 		}
 		// A specific case where we add the first index if matched
-		if (countA.equals(countB)) {
+		if (countA.equals(countDic))
 			ans.add(0);
-		}
-		// We declare the sliding window pointer
-		int slidingLeft, right;
-		slidingLeft = 0;
-		right = b.length();
-		while (right<a.length()) {
-			if (!countA.containsKey(a.charAt(right))) countA.put(a.charAt(right), 1);
-			else countA.put(a.charAt(right), countA.get(a.charAt(right)) + 1);
-
-			countA.put(a.charAt(slidingLeft), countA.get(a.charAt(slidingLeft)) - 1);
-			if (countA.get(a.charAt(slidingLeft)) == 0) countA.remove(a.charAt(slidingLeft));
-			slidingLeft++;
+		int Left = 0, right = dic.length();
+		while (right < a.length()) {
+			countA.put(a.charAt(right), countA.getOrDefault(a.charAt(right), 0) + 1);
+			countA.put(a.charAt(Left), countA.get(a.charAt(Left)) - 1);
+			if (countA.get(a.charAt(Left)) == 0)
+                countA.remove(a.charAt(Left));
+			Left++;
 			right++;
-			if (countA.equals(countB)) {
-				// If all the characters from b with their respective frequency are found at this index, we append.
-				ans.add(slidingLeft);
+			if (countA.equals(countDic)) {
+				ans.add(Left);
 			}
 		}
 		return ans; 
