ec3690a834e1da8222d63afe7167475f98efd82b
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-29 14:21:48 +0000

    Time: 2 ms (71.33%), Space: 40.8 MB (27.43%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,35 +1,19 @@
-/*
-Understanding:
-    "((}}[{)}]))"
-    ({)}->false
-    {}
-    []
-Target:
-    return true or false
-Solution:
-    dic:Key(->value-> )
-        Key{->value-> }
-        Key[->value-> ]
-    check->left then ->right ...store->left then ->right
-*/
 class Solution {
     public boolean isValid(String s) {
-        Map<Character,Character> dic=new HashMap<>();
-        Stack<Character> stack=new Stack<>();
-        dic.put(')','(');
-        dic.put(']','[');
-        dic.put('}','{');
-        for(char c:s.toCharArray()){
-            if(dic.containsKey(c)){
-                if(!stack.isEmpty() && dic.get(c)==stack.peek()){
-                    stack.pop();
-                }else
+        Map<Character,Character> map = new HashMap<>();
+        map.put(')','(');
+        map.put(']','[');
+        map.put('}','{');
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0 ; i < s.length() ; i++){
+            char c = s.charAt(i);
+            if(map.containsKey(c)){
+                if(stack.isEmpty() || stack.pop() != map.get(c))
                     return false;
-            }else
+            }
+            else
                 stack.push(c);
         }
-        if(stack.isEmpty())
-            return true;
-        return false;
+        return stack.isEmpty();
     }
 }
