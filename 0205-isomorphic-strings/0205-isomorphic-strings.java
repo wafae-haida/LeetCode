2a2fe37bc4559baa7a5bb95ef7751db5a1adfdc1
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-29 23:01:09 +0000

    Time: 23 ms (15.91%), Space: 42.7 MB (29.17%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,14 +1,25 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
+        
         Map<Character,Character> sourceToDes=new HashMap<>();
         Map<Character,Character> desToSource=new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            char source=s.charAt(i),dest=t.charAt(i);
-            char sourceResult=sourceToDes.getOrDefault(source,dest);
-            if(sourceResult!=dest) return false;
+        
+        for(int i = 0; i < s.length() ; i++){
+            char source = s.charAt(i);
+            char dest = t.charAt(i);
+            
+            char sourceResult = sourceToDes.getOrDefault(source,dest);
+            
+            if(sourceResult != dest) 
+                return false;
+            
             sourceToDes.put(source,sourceResult);
-            char destReturn=desToSource.getOrDefault(dest,source);
-            if(destReturn!=source) return false;
+            
+            char destReturn = desToSource.getOrDefault(dest,source);
+            
+            if(destReturn != source) 
+                return false;
+            
             desToSource.put(dest,destReturn);
         }
         return true;
