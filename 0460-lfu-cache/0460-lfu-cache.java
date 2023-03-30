a7925c00bd4983743aba99857d08bd5dad36923a
wafaehai <95100809+wafaehai@users.noreply.github.com>
2023-03-30 16:16:05 +0000

    Time: 69 ms (69.72%), Space: 117.8 MB (94.71%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.java
+++ 0460-lfu-cache/0460-lfu-cache.java
@@ -1,71 +1,51 @@
 class LFUCache {
-    // key: original key, value: frequency and original value.
-    private Map<Integer, Pair<Integer, Integer>> cache;
-    // key: frequency, value: All keys that have the same frequency.
-    private Map<Integer, LinkedHashSet<Integer>> frequencies;
-    private int minf;
-    private int capacity;
-    
-    private void insert(int key, int frequency, int value) {
-        cache.put(key, new Pair<>(frequency, value));
-        frequencies.putIfAbsent(frequency, new LinkedHashSet<>());
-        frequencies.get(frequency).add(key);
-    }
+    E[] map=new E[100_001];
+    LinkedHashSet<E>[] freqBuckets= new LinkedHashSet[10_001];
+    int lf=0,capacity;
+    boolean block=false;
 
     public LFUCache(int capacity) {
-        cache = new HashMap<>();
-        frequencies = new HashMap<>();
-        minf = 0;
-        this.capacity = capacity;
+        this.capacity=capacity;
+        if(capacity==0) block=true;
     }
     
     public int get(int key) {
-        Pair<Integer, Integer> frequencyAndValue = cache.get(key);
-        if (frequencyAndValue == null) {
-            return -1;
-        }
-        final int frequency = frequencyAndValue.getKey();
-        final Set<Integer> keys = frequencies.get(frequency);
-        keys.remove(key);
-        if (keys.isEmpty()) {
-            frequencies.remove(frequency);
-            
-            if (minf == frequency) {
-                ++minf;
-            }
-        }
-        final int value = frequencyAndValue.getValue();
-        insert(key, frequency + 1, value);   
-        return value;
+        if(block) return -1;
+        E v=map[key];
+        if(v==null) return -1;
+        updateFreq(v);
+        return v.value;
     }
     
     public void put(int key, int value) {
-        if (capacity <= 0) {
-            return;
-        }
-        Pair<Integer, Integer> frequencyAndValue = cache.get(key);
-        if (frequencyAndValue != null) {
-            cache.put(key, new Pair<>(frequencyAndValue.getKey(), value));
-            get(key);
-            return;
-        }
-        if (capacity == cache.size()) {
-            final Set<Integer> keys = frequencies.get(minf);
-            final int keyToDelete = keys.iterator().next();
-            cache.remove(keyToDelete);
-            keys.remove(keyToDelete);
-            if (keys.isEmpty()) {
-                frequencies.remove(minf);
+        if(block) return;
+        E v=map[key];
+        if(v==null) {
+            if(capacity==0) {
+                E r=freqBuckets[lf].iterator().next();
+                freqBuckets[lf].remove(r);
+                map[r.key]=null;
+                capacity++;
             }
+            v=new E(key,value);
+            capacity--;
+        } else v.value=value;
+        updateFreq(v);
+        map[key]=v;
+    }
+
+    private void updateFreq(E keyE) {
+        if(keyE.f==0||(keyE.f==lf&&freqBuckets[keyE.f].size()==1)) lf=keyE.f+1;
+        if(keyE.f++!=0) freqBuckets[keyE.f-1].remove(keyE);
+        if(freqBuckets[keyE.f]==null) freqBuckets[keyE.f]=new LinkedHashSet<>();
+        freqBuckets[keyE.f].add(keyE);
+    }
+
+    class E {
+        int f=0,key,value;
+        public E(int key, int value) {
+            this.key=key;
+            this.value=value;
         }
-        minf = 1;
-        insert(key, 1, value);
     }
 }
-
-/**
- * Your LFUCache object will be instantiated and called as such:
- * LFUCache obj = new LFUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
